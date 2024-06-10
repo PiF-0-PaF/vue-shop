b88011b46685a00a730290d0b7b747891a06e5a2
Ian Vinokurov <72916820+PiF-0-PaF@users.noreply.github.com>
2024-06-10 05:46:56 +0500

    Add files via upload

---
+++ src/components/Card.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { inject } from 'vue'
+
+const props = defineProps({
+  id: Number,
+  title: String,
+  imageUrl: String,
+  price: Number,
+  isFavourite: Boolean,
+  isAdded: Boolean,
+  onClickFavorite: Function
+})
+
+const addToFavorite = inject('addToFavorite')
+
+const onClickFavorite = () => {
+  const obj = {
+    parentId: props.id
+  }
+
+  addToFavorite(obj)
+}
+</script>
+
+<template>
+  <div
+    class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer hover:-translate-y-2 transition hover:shadow-xl"
+  >
+    <img
+      :src="!isFavourite ? '/like-1.svg' : '/like-2.svg'"
+      alt="Like-1"
+      class="absolute top-8 left-8"
+      @click="onClickFavorite"
+    />
+
+    <img :src="imageUrl" alt="Sneaker" />
+
+    <p class="mt-2">{{ title }}</p>
+
+    <div class="flex justify-between mt-5">
+      <div class="flex flex-col">
+        <span class="text-slate-400">Цена:</span>
+        <b>{{ price }}</b>
+      </div>
+
+      <img @click="onClickAdd" :src="!isAdded ? '/plus.svg' : '/checked.svg'" alt="Plus" />
+    </div>
+  </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="flex justify-between border-b border-slate-200 px-10 py-8">
+    <div class="flex items-center gap-4">
+      <img src="/logo.png" alt="Logo" class="w-10 mr-5" />
+      <div>
+        <h2 class="text-xl font-bold uppercase">Prod.Comp</h2>
+        <p class="text-slate-500">Магазин лучшей техники</p>
+      </div>
+    </div>
+
+    <ul class="flex items-center gap-10">
+      <li class="flex items-center gap-3 text-slate-500 hover:text-black cursor-pointer">
+        <img src="/cart.svg" alt="Cart" />
+        <b>1200 руб.</b>
+      </li>
+
+      <li class="flex items-center gap-3 text-slate-500 hover:text-black cursor-pointer">
+        <img src="/heart.svg" alt="Heart" />
+        <span>Закладки</span>
+      </li>
+
+      <li class="flex items-center gap-3 text-slate-500 hover:text-black cursor-pointer">
+        <img src="/profile.svg" alt="Profile" />
+        <span>Профиль</span>
+      </li>
+    </ul>
+  </header>
+</template>
